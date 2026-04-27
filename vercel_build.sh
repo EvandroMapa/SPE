d160660c98ab7b363641d551be0732a18ba038c2
Evandro <evandromapa@gmail.com>
2026-04-27 16:16:04 -0300

    fix: verbosity and analytics

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -x
 echo "Baixando o Flutter SDK..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
-echo "Habilitando Flutter Web..."
+echo "Configurando Flutter..."
+flutter config --no-analytics
 flutter config --enable-web
 
 echo "Baixando dependências..."
